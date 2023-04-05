8d6e6aeea5d6bf06ddd74be5462c2c93d4dface1
alencabira <dbalencastro@gmail.com>
2023-04-05 15:31:05 -0300

    js

--- Gruntfile.js
+++ Gruntfile.js
@@ -105,5 +105,5 @@ module.exports = function(grunt){
     
 
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'clean']);
 }
